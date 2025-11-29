4f24cdb2df9ac39b87cd0f1a1c90aa80461c2861
Brian Bauska <brianbauska@gmail.com>
2025-11-29 15:22:33 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 /* /js/scripts.js in github Silus-Counter-2 making silus-counter-2.bauska.org */
-/* Nov 29, 2025 = xx,### (xx hours approx)
+/* Nov 28, 2025 = 5,000 (xx hours approx)
 */
 let counter = 0;
-/* xx,xxx Nov 28, 2025
+/* 5,000 Nov 28, 2025
   all times are approximate. */
 
 function count() {
